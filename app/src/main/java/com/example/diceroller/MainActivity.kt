8da5a7622c8fa0522f1afa78e78fd9e2d5b02d0d
Taren <taren.lewis@gmail.com>
2020-05-15 13:36:08 -0500

    Working version 1.0 Dice Roller, not refactored for good coding practice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +19,24 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Lets Roll"
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val randomNum = Random.nextInt(6) + 1
+
+        val drawableResource = when (randomNum) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
+
     }
 }
